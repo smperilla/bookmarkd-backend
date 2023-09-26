093cccd13b356e2bc9e34a5cb601f6e076191a3d
sperilla <smperilla@gmail.com>
2023-09-25 23:30:31 -0400

    working full working INDUCES backend

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -6,22 +6,52 @@ const Bookmark = require("../models/Bookmark");
 
 //INDEX
 router.get("/", async (req, res) => {
-    try {
-        res.json(await Bookmark.find());
-    } catch (error) {
-        res.status(400).json(error);
-    }
+  try {
+    res.json(await Bookmark.find());
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 //CREATE
 
-router.post("/",async (req, res) => {
-    try {
-        res.json(await Bookmark.create(req.body));
-    } catch (error) {
-        res.status(400).json(error);
-    }
+router.post("/", async (req, res) => {
+  try {
+    res.json(await Bookmark.create(req.body));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// SHOW
+router.get("/:id", async (req, res) => {
+  try {
+    res.json(await Bookmark.findById(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    res.json(await Bookmark.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    res.json(
+      await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
-
